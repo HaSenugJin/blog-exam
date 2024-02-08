066e21df25a369601555ef578b08f851d8ee7101
egdg4587 <egdg7777@gmail.com>
2024-02-08 16:58:10 +0900

    심화1번끝

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,5 +1,5 @@
-insert into board_tb(title, content, author, created_at) values('제목1', '내용1', '0/11/1', now());
-insert into board_tb(title, content, author, created_at) values('제목2', '내용2', '이젠', now());
-insert into board_tb(title, content, author, created_at) values('제목3', '내용3', '야스오', now());
-insert into board_tb(title, content, author, created_at) values('제목4', '내용4', '0/10/1', now());
-insert into board_tb(title, content, author, created_at) values('제목5', '내용5', '우리팀', now());
+insert into board_tb(title, content, author, created_at) values('제목1', '내용1', '홍길동', now());
+insert into board_tb(title, content, author, created_at) values('제목2', '내용2', '임꺽정', now());
+insert into board_tb(title, content, author, created_at) values('제목3', '내용3', '이방원', now());
+insert into board_tb(title, content, author, created_at) values('제목4', '내용4', '성춘향', now());
+insert into board_tb(title, content, author, created_at) values('제목5', '내용5', '하승진', now());
